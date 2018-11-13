e4114f12ad129e54e507cf760579a159aa5295d3
Robert Berryhill <robjberryhill@gmail.com>
2018-11-13 15:34:06 -0600

    Deployment Adjusted

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
 app.use(function(req, res, next) {
+  /*res.header("Access-Control-Allow-Origin", "http://ec2-3-16-37-2.us-east-2.compute.amazonaws.com");*/
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '13.59.209.83',
+      host: '3.16.37.2',
       username: 'ubuntu',
       privateKey: 'ba-key.pem'
     })
